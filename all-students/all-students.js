ae9f3364bf7e0cfabf46aeb4c902d92ab044cc3d
dean <nicklasfrederiksen@gmail.com>
2025-02-27 15:42:39 +0100

    First

---
+++ all-students/all-students.js
@@ -0,0 +1,38 @@
+document.querySelector('#btnCreate').addEventListener('click', () =>{
+    // Gather form values
+    const name = document.querySelector('#name').value;
+    const age = parseInt(document.querySelector('#age').value);
+
+    // Convert the radio value to a boolean
+    const isActive = document.querySelector('input[name="active"]:checked').value === 'true';
+
+    const data = { name, age, isActive };
+    console.log(data);
+
+    fetch('http://localhost:8080/students/add', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => {
+        console.log('Response Success:', result);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
+
+
+function logAllStudents(){
+    fetch("http://localhost:8080/students/all")
+        .then(response =>{
+            return response.json();
+        })
+        .then(json => console.log(json));
+}
+
+
